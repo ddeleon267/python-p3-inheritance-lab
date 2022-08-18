f2da9b385dae62cb39fc8cc85aacabec1d1c2391
ddeleon267 <dominique.a.deleon@gmail.com>
2022-08-18 11:20:03 -0400

    Done

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,15 @@
 
 from lib.user import User
 
+#!/usr/bin/env python
+
+from lib.user import User
+
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, str):
+        self.knowledge.append(str)
